d71e03129a6c281eb838a64c9e94573b44e797df
Thenaesh Elango <thenaeshelango@gmail.com>
2016-11-15 11:48:04 +0800

    add autoclosing to Lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -28,31 +28,31 @@ contract Lottery
       pool += tixPrice;
       if (msg.value > tixPrice) msg.sender.send(msg.value - tixPrice);
     }
+
+    if (players.length > 100) closeLottery();
   }
 
   function generateRandomNumber() internal returns (uint) {
     return 8888;
   }
 
-  function closeLottery() {
-    if (msg.sender == owner) {
-      uint winningnum = generateRandomNumber();
+  function closeLottery() internal {
+    uint winningnum = generateRandomNumber();
 
-      Player[] winners;
-      for (uint i = 0; i < players.length; i++) {
-        if (players[i].tixnum == winningnum) winners.push(players[i]);
+    Player[] winners;
+    for (uint i = 0; i < players.length; i++) {
+      if (players[i].tixnum == winningnum) winners.push(players[i]);
+    }
+
+    if (players.length > 100 && winners.length > 0) {
+      pool = pool * (100 - commission) / 100;
+      uint prize_per_winner = pool / winners.length;
+
+      for (uint j = 0; j < winners.length; j++) {
+        winners[j].addr.send(prize_per_winner);
       }
 
-      if (winners.length > 0) {
-        pool = pool * (100 - commission) / 100;
-        uint prize_per_winner = pool / winners.length;
-
-        for (uint j = 0; j < winners.length; j++) {
-          winners[j].addr.send(prize_per_winner);
-        }
-
-        selfdestruct(owner);
-      }
+      selfdestruct(owner);
     }
   }
 }
